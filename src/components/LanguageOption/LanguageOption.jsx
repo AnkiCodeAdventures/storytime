508aea0f155185c913d7caf4fe37065a96883a5e
Ankita Agarwal <ankitaag94@gmail.com>
2023-11-28 15:17:01 +0530

    add icon to language componet

--- src/components/LanguageOption/LanguageOption.jsx
+++ src/components/LanguageOption/LanguageOption.jsx
@@ -2,10 +2,17 @@ import styles from "./LanguageOption.module.css";
 
 function LanguageOption() {
   return (
-    <select name="language" className={styles.language}>
-      <option value="english">English</option>
-      <option value="hindi">हिंदी</option>
-    </select>
+    <div className={styles.wrapper}>
+      <img
+        className={styles.icon}
+        src="/public/assets/language.svg"
+        alt="language"
+      />
+      <select name="language" className={styles.language}>
+        <option value="english">English</option>
+        <option value="hindi">हिंदी</option>
+      </select>
+    </div>
   );
 }
 
